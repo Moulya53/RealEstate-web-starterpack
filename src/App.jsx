415ce46b1ad4db922f81d20f93413455a39a4535
Moulya53 <4nn21cs027@nieit.ac.in>
2024-02-19 08:31:35 +0530

    modified:   src/App.jsx 	new file:   src/app.css 	new file:   src/components/Residencies/Residencies.css 	new file:   src/components/Residencies/Residencies.jsx 	modified:   src/components/header/header.css 	modified:   src/components/hero/hero.css 	new file:   src/utils/common.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import header from "./components/header/header";
-import { hero } from "./components/hero/hero";
+import hero from "./components/hero/hero";
+import "./app.css"
+import { Residencies } from "./components/Residencies/Residencies";
+
 function App() {
   return (
     <div className="App">
+      <div>
+        <div className="white-gradient" />
       <header />
       <hero />
+      </div>
+      <Residencies/>
     </div>
   );
 }
